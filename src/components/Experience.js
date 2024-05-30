0a704b16c28b299295fe2af7f9de13093aa7b20c
RajeshManoharan92 <m.rajeshmanohar@gmail.com>
2024-05-30 18:40:58 +0530

    portfolio added

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -43,10 +43,26 @@ tasks on time.<br></br>
                                                 •	Monitoring and improving front-end performance. <br></br>
                                             </Card.Text>
                                             <Card.Title style={{"color":"blue"}}>Projects  </Card.Title>
+
+
+                                            <h6 style={{"color":"darkgreen"}}><u>Manual</u></h6>
+                                            <Card.Text>
+                                            •	Knowledge sharing tool for Indusind IT team.
+                                                <h6 style={{"color":"darkgreen"}}>Roles & Responsibilities
+</h6>
+•	Currently developing "Manual," a knowledge-sharing tool, as the sole developer. <br></br>
+•	Released Version 1 in record time, now in use by the IndusInd IT team. <br></br>
+•	Developing Version 2 with a front end in React, a back end in NestJS, and CouchDB as the database.<br></br>
+•	Collaborate with the project head to gather requirements, design both front-end and back-end logic, and implement necessary changes to enhance the tool's functionality.
+
+                                            </Card.Text>
+
+
+
                                             <h6 style={{"color":"darkgreen"}}><u>Indusind Bank Data Migration</u></h6>
                                             <Card.Text>
-                                            • Indusind Bank migrating its data from Finacle to Couch Db.
-                                            • Backend development using Node JS, Azure Event Hub NPM package, Couch DB. 
+                                            • Indusind Bank migrating its data from Finacle to Couch Db.<br></br>
+                                            • Backend development using Node JS, Azure Event Hub NPM package, Couch DB. <br></br>
                                                 <h6 style={{"color":"darkgreen"}}>Roles & Responsibilities
 </h6>
                                                 •	In my professional capacity, I am tasked with extracting data from Azure Event 
@@ -60,19 +76,19 @@ Node.js code to efficiently manage this data transfer process
 
                                             <Card.Text>
                                                 •	Revamp of Net Banking & Mobile Banking Login API’s with newly integrated 
-Couch DB
+Couch DB<br></br>
 • I received an acknowledgment email from the client expressing appreciation for 
-my contributions to the aforementioned project. 
-• Backend API’s development using Nest JS framework, Node JS, Couch DB
+my contributions to the aforementioned project. <br></br>
+• Backend API’s development using Nest JS framework, Node JS, Couch DB<br></br>
                                                 <h6 style={{"color":"darkgreen"}}>Roles & Responsibilities</h6>
                                                 •	I spearheaded the research and development efforts for the Nest.js framework, 
 culminating in the creation of a pivotal project for the revamp of Net Banking 
-and Mobile Banking (NBMB)
+and Mobile Banking (NBMB)<br></br>
 • In this role, I successfully developed APIs utilizing the Nest.js framework in 
 conjunction with CouchDB, contributing significantly to the success of the 
-Revamp NBMB project.
+Revamp NBMB project.<br></br>
 • I collaborated closely with the front-end team, gathering project requirements and subsequently developed 
-APIs in alignment with their specifications.
+APIs in alignment with their specifications.<br></br>
 • Assumed the role of mentoring a new team member who joined as a fresher, providing guidance and 
 conducting training sessions to align her skills with project requirements
                                             </Card.Text>
@@ -81,14 +97,14 @@ conducting training sessions to align her skills with project requirements
 
 <Card.Text>
     •	Single Sign-On (SSO) project aimed at implementing IndusInd SSO for the Rapipay partner, enhancing 
-security and user authentication processes. 
-• Frontend API’s development using React JS, HTML 5, CSS, Material UI.
+security and user authentication processes. <br></br>
+• Frontend API’s development using React JS, HTML 5, CSS, Material UI.<br></br>
     <h6 style={{"color":"darkgreen"}}>Roles & Responsibilities</h6>
-    •	Product Owner for React JS module
+    •	Product Owner for React JS module<br></br>
     • Conducted in-depth research and exploration on Single Sign-On (SSO) methodologies, with a 
-specific focus on implementing SSO using React applications.
+specific focus on implementing SSO using React applications.<br></br>
 • Designed and developed React.js components for Single Sign-On (SSO), successfully 
-implementing the solution in a production environment where it is operating seamlessly
+implementing the solution in a production environment where it is operating seamlessly<br></br>
 • Enhancing the existing implementation of the system, which incorporates Aadhar card OTP 
 verification, by further refining it to include PAN-Aadhar linkage, thereby improving the overall 
 functionality and security of the system.
@@ -98,8 +114,8 @@ functionality and security of the system.
 <h6 style={{"color":"darkgreen"}} ><u>Magnati Mobile APP – React Ionic</u></h6>
 
 <Card.Text>
-    •	 Magnati is a Leading trusted payment solution company in UAE
- Frontend API’s development using React Ionic Framework,.
+    •	 Magnati is a Leading trusted payment solution company in UAE <br></br>
+    • Frontend API’s development using React Ionic Framework,. <br></br>
     <h6 style={{"color":"darkgreen"}}>Roles & Responsibilities</h6>
     •	Developed a Proof of Concept (POC) for the React Ionic mobile application, showcasing the feasibility and 
 potential of the application for the Magnati client.
